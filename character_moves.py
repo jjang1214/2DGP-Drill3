1804e06d3a734301674724502d8fb36a5cfa3ef1
J1214 <horse121464@gmail.com>
2025-09-17 12:13:43 +0900

    load & draw image

--- character_moves.py
+++ character_moves.py
@@ -2,19 +2,27 @@ from pico2d import *
 
 open_canvas()
 
+grass=load_image('grass.png')
+character=load_image('character.png')
 
+character.draw_now(400, 90)
+grass.draw_now(400, 30)
 
 def move_rectangle():
+    #clear_canvas_now()
     pass
 
 
 
+
 def move_triangle():
+    #clear_canvas_now()
     pass
 
 
 
 def move_circle():
+    #clear_canvas_now()
     pass
 
 
