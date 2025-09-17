885346c6e877e940605149d03518f2e84d7db397
J1214 <horse121464@gmail.com>
2025-09-17 12:24:37 +0900

    캐릭터의 기본 정보 설정

--- character_moves.py
+++ character_moves.py
@@ -8,6 +8,13 @@ character=load_image('character.png')
 character_x=400
 character_y=90
 
+character_width=40
+character_height=80
+
+character_speed=2
+
+
+
 character.draw_now(character_x, character_y)
 grass.draw_now(400, 30)
 
