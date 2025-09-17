ea227de7310e37462edb6bad241c7cc29a969923
J1214 <horse121464@gmail.com>
2025-09-17 12:49:26 +0900

    그리기 함수 생성

--- character_moves.py
+++ character_moves.py
@@ -21,40 +21,34 @@ character_speed=2
 character.draw_now(character_x, character_y)
 grass.draw_now(glass_x, glass_y)
 
+def clear_and_draw():
+    clear_canvas_now()
+    grass.draw_now(glass_x, glass_y)
+    character.draw_now(character_x, character_y)
+    delay(0.01)
+
 def move_rectangle():
     global character_x, character_y
 
     if character_x + character_width/2 < 800 and character_y == 90:
         character_x += character_speed
 
-        clear_canvas_now()
-        grass.draw_now(glass_x, glass_y)
-        character.draw_now(character_x, character_y)
-        delay(0.01)
+        clear_and_draw()
 
     elif character_x + character_width/2 == 800 and character_y + character_height/2 < 600:
         character_y += character_speed
 
-        clear_canvas_now()
-        grass.draw_now(glass_x, glass_y)
-        character.draw_now(character_x, character_y)
-        delay(0.01)
+        clear_and_draw()
 
     elif character_x - character_width/2 > 0 and character_y + character_height/2 == 600:
         character_x -= character_speed
 
-        clear_canvas_now()
-        grass.draw_now(glass_x, glass_y)
-        character.draw_now(character_x, character_y)
-        delay(0.01)
+        clear_and_draw()
 
     elif character_x - character_width/2 == 0 and character_y > 90:
         character_y -= character_speed
 
-        clear_canvas_now()
-        grass.draw_now(glass_x, glass_y)
-        character.draw_now(character_x, character_y)
-        delay(0.01)
+        clear_and_draw()
 
 
 
