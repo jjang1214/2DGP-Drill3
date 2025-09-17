69d4589d6eddea50341c5ab8e58930c2e4e3ccc0
J1214 <horse121464@gmail.com>
2025-09-17 12:03:34 +0900

    사각운동, 삼각운동, 원운동 함수 뼈대 완성

--- character_moves.py
+++ character_moves.py
@@ -2,5 +2,25 @@ from pico2d import *
 
 open_canvas()
 
-while True:
+
+
+def move_rectangle():
     pass
+
+
+
+def move_triangle():
+    pass
+
+
+
+def move_circle():
+    pass
+
+
+
+
+while True:
+    move_rectangle()
+    move_triangle()
+    move_circle()
